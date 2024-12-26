ee1b2cfbbd0696b4144eb84b355b098538e5af8b
Arthur Henrique Zarpelon Kunz <arthurhenriquezarpelonkunz@MacBook-Air-de-Arthur.local>
2024-12-26 20:37:35 -0300

    chore: add prisma index

--- auth-service/src/infra/database/prisma.ts
+++ auth-service/src/infra/database/prisma.ts
@@ -2,8 +2,4 @@ import { PrismaClient } from "@prisma/client"
 
 const prisma = new PrismaClient()
 
-const prismaConnection = async () => {
-  await prisma.$connect().then(() => console.log("Connected to database"))
-}
-
-export default prismaConnection
+export default prisma
